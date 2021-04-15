4d6d446d5bcbfe3fbac0bb2abff0a4f855dc5307
gDelps <guilhermedelfino25@gmail.com>
2021-04-15 14:12:38 -0300

    index e menu finalizado

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use(express.static(__dirname+'./assets/'))
+app.use(express.static(__dirname+'/assets/'))
 
 app.set('view engine','ejs');
 app.set('views', __dirname+'/assets/views');
@@ -23,6 +23,18 @@ app.get('/',(req,res)=>{
     res.render('index.ejs',{title:'Home'})
 })
 
+app.get('/create',(req,res)=>{
+    res.render('create.ejs',{title:'Create'})
+})
+app.get('/read',(req,res)=>{
+    res.render('read.ejs',{title:'Read'})
+})
+app.get('/update',(req,res)=>{
+    res.render('update.ejs',{title:'Update'})
+})
+app.get('/delete',(req,res)=>{
+    res.render('delete.ejs',{title:'Delete'})
+})
 
 
 
